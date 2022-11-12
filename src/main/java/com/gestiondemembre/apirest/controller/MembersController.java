71e06ba8a62b659872a2630d1baff320d6596a76
miharyjoel <miharyjoel@gmail.com>
2022-11-12 09:13:47 +0300

    feat: add get Members api

--- src/main/java/com/gestiondemembre/apirest/controller/MembersController.java
+++ src/main/java/com/gestiondemembre/apirest/controller/MembersController.java
@@ -1,13 +1,27 @@
 package com.gestiondemembre.apirest.controller;
 
+import com.gestiondemembre.apirest.model.Members;
+import com.gestiondemembre.apirest.service.MembersService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @AllArgsConstructor
 @RequestMapping("/members")
 @CrossOrigin
 public class MembersController {
+
+    private final MembersService service;
+    @GetMapping("")
+    public List<Members> getAllDishes(@RequestParam Optional<Integer> page, @RequestParam Optional<Integer> pageSize){
+        return service.getAllDishes(page, pageSize);
+    }
+
 }
--- src/main/java/com/gestiondemembre/apirest/service/MembersService.java
+++ src/main/java/com/gestiondemembre/apirest/service/MembersService.java
@@ -1,9 +1,25 @@
 package com.gestiondemembre.apirest.service;
 
+import com.gestiondemembre.apirest.model.Members;
+import com.gestiondemembre.apirest.repository.MembersRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
 public class MembersService {
+
+    private final MembersRepository  repository;
+
+    public List<Members> getAllDishes (Optional<Integer> page, Optional<Integer> pageSize){
+        Pageable pageable = PageRequest.of(page.orElse(0), pageSize.orElse(10));
+        return  repository.findAll(pageable).stream().toList();
+
+    }
+
 }
